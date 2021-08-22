748a7e12c72e1d888ce807c57d2f9a0c9964bc86
”СергейБейфусgit config --global user.email 0legdamagarov@gmail.com$ git config --global user.name ”СергейБейфус <0legdamagarov@gmail.com>
2021-08-22 15:54:30 +0600

    Java_01_-_Prosteyshie_zadachi задание 5

--- src/com/levelp/jb2/HelloWorld1.java
+++ src/com/levelp/jb2/HelloWorld1.java
@@ -6,23 +6,15 @@ public class HelloWorld1 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите три вещественных числа");
+        System.out.println("Введите два числа ввиде промежутка [a,b]");
         double num1 = in.nextDouble();
         double num2 = in.nextDouble();
+
+        System.out.println("Введите число, чтобы задать шаг N");
         double num3 = in.nextDouble();
-        double Discriminant = num2*num2 - 4*num1*num3;
-        if (Discriminant < 0){
-            System.out.println("Корней нет");
-        }
-        else if(Discriminant == 0){
-            double X = (-1*num2)/(2*num1);
-            System.out.println("X: " + X);
-        }
-        else{
-            double X1 = ((-1*num2)-(Math.sqrt(Discriminant)))/(2*num1);
-            double X2 = ((-1*num2)+(Math.sqrt(Discriminant)))/(2*num1);
-            System.out.println("X1: " + X1);
-            System.out.println("X2: " + X2);
+
+        for (double i = num1; i <= num2; i += num3) {
+            System.out.printf("%.0f  %.5f \n", i, ((Math.sqrt(i-2)+1)/i-2));
         }
     }
 }
