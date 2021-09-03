964634799dd6c086dd24f88ef32885ffeadbff5f
”СергейБейфусgit config --global user.email 0legdamagarov@gmail.com$ git config --global user.name ”СергейБейфус <0legdamagarov@gmail.com>
2021-09-03 19:38:46 +0600

    Java_01_-_Prosteyshie_zadachi задание 6

--- src/com/levelp/jb2/HelloWorld1.java
+++ src/com/levelp/jb2/HelloWorld1.java
@@ -6,15 +6,30 @@ public class HelloWorld1 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите два числа ввиде промежутка [a,b]");
-        double num1 = in.nextDouble();
-        double num2 = in.nextDouble();
+        System.out.println("имеется система уравнений вида: / а1*х1 + а2*х2 + а3 = 0");
+        System.out.println("                                \\ b1*х1 + b2*х2 + b3 = 0");
+        System.out.println("Введите: а1;а2;а3;b1;b2;b3 , чтобы найти: х1;x2");
 
-        System.out.println("Введите число, чтобы задать шаг N");
-        double num3 = in.nextDouble();
+        double a1 = in.nextDouble();
+        double a2 = in.nextDouble();
+        double a3 = in.nextDouble();
 
-        for (double i = num1; i <= num2; i += num3) {
-            System.out.printf("%.0f  %.5f \n", i, ((Math.sqrt(i-2)+1)/i-2));
+        double b1 = in.nextDouble();
+        double b2 = in.nextDouble();
+        double b3 = in.nextDouble();
+
+        if((a1/b1 == a2/b2) && (a2/b2 != a3/b3)){
+            System.out.println(" Решений нет!");
+            }
+        else if((a1/b1 == a2/b2) && (a2/b2 == a3/b3)){
+            System.out.println(" Решений бесконечное множество.");
+        }
+        else if(a1/b1 != a2/b2){
+
+            double x1,x2;
+            x1=(a3*b2-a2*b3)/(a1*b2-a2*b1);
+            x2=(a1*b3-a3*b1)/(a1*b2-a2*b1);
+            System.out.println("x1 =" + x1 + ", x2 =" + x2);
         }
     }
 }
