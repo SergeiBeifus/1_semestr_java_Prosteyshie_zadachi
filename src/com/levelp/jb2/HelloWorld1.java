45a1e7b2b53421b7cc4e6de984a2b8061e56851d
”СергейБейфусgit config --global user.email 0legdamagarov@gmail.com$ git config --global user.name ”СергейБейфус <0legdamagarov@gmail.com>
2021-09-04 15:52:07 +0600

    Java_01_-_Prosteyshie_zadachi задание 7

--- src/com/levelp/jb2/HelloWorld1.java
+++ src/com/levelp/jb2/HelloWorld1.java
@@ -2,34 +2,38 @@ package com.levelp.jb2;
 import java.util.Scanner;
 import java.awt.*;
 
+
 public class HelloWorld1 {
+
+    public static int factorial(int n)
+    {
+        if (n == 0) return 1;
+        return n * factorial(n-1);
+    }
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
-        System.out.println("имеется система уравнений вида: / а1*х1 + а2*х2 + а3 = 0");
-        System.out.println("                                \\ b1*х1 + b2*х2 + b3 = 0");
-        System.out.println("Введите: а1;а2;а3;b1;b2;b3 , чтобы найти: х1;x2");
+        double exp=0;
 
-        double a1 = in.nextDouble();
-        double a2 = in.nextDouble();
-        double a3 = in.nextDouble();
+        System.out.println("Введите х");
+        double x = 1.0;
+        x = in.nextDouble();
 
-        double b1 = in.nextDouble();
-        double b2 = in.nextDouble();
-        double b3 = in.nextDouble();
+        System.out.println("Введите точность");
+        double eps = 1.0;
+        eps = in.nextDouble();
 
-        if((a1/b1 == a2/b2) && (a2/b2 != a3/b3)){
-            System.out.println(" Решений нет!");
-            }
-        else if((a1/b1 == a2/b2) && (a2/b2 == a3/b3)){
-            System.out.println(" Решений бесконечное множество.");
+        double fctrl = 1;
+        double trm =1 ;
+        for(int i = 1;trm >= eps;i++){
+
+            exp = exp + trm;
+            fctrl = factorial(i);
+            trm = (Math.pow(x,i)/fctrl);
+
         }
-        else if(a1/b1 != a2/b2){
-
-            double x1,x2;
-            x1=(a3*b2-a2*b3)/(a1*b2-a2*b1);
-            x2=(a1*b3-a3*b1)/(a1*b2-a2*b1);
-            System.out.println("x1 =" + x1 + ", x2 =" + x2);
-        }
+        System.out.println(exp);
     }
 }
+
